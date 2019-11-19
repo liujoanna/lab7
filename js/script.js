5bdf09e652e7c828a83bf172ac5ffade751b17c2
liujoanna <jliu126@hawk.iit.edu>
2019-11-19 11:52:07 -0600

    formatted js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-function init(){
-//add your javascrip between these two lines of code
-var click = document.getElementById("entrybutton");
-click.addEventListener("click",function(){
-  var input = document.getElementById("entryinput")
-  var output = document.getElementById("textoutput");
+function init() {
+   //add your javascrip between these two lines of code
+   var click = document.getElementById("entrybutton");
+   click.addEventListener("click", function () {
+      var input = document.getElementById("entryinput")
+      var output = document.getElementById("textoutput");
 
-  output.innerHTML = input.value;
+      output.innerHTML = input.value;
 
-  alert("Joanna Liu:" + input.value);
-});
+      alert("Joanna Liu:" + input.value);
+   });
 }
 
 window.addEventListener('load', init);
