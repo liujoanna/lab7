a2f7f6bde6a1896d76575d647d00bcc00b84920b
liujoanna <jliu126@hawk.iit.edu>
2019-11-19 11:46:10 -0600

    added script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+var click = document.getElementById("entrybutton");
+click.addEventListener("click",function(){
+  var input = document.getElementById("entryinput")
+  var output = document.getElementById("textoutput");
 
+  output.innerHTML = input.value;
 
-
-
-
-
+  alert("Joanna Liu:" + input.value);
+});
 
 window.addEventListener('load', init);
